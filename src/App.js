68917b5015ae33208982fbcb2cfc3838f65f1303
HassanKhan123 <hk2602809@gmail.com>
2020-07-24 23:40:29 +0500

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/ui/Header'
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-     hello
+    <div className="container">
+     <Header />
      
     </div>
   );
---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <div className="center">
+            <img src={Logo} alt=""/>
+        </div>
+    )
+}
+
+export default Header
