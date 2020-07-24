0f5519c52104c71551268c3bc3ec8b78df54c6b2
HassanKhan123 <hk2602809@gmail.com>
2020-07-24 23:57:32 +0500

    fetching characters info via api call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import Header from './components/ui/Header'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import CharacterGrid from './components/characters/CharactersGrid'
+import "./App.css";
+import Loading from "./images/spinner.gif";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
   return (
     <div className="container">
-     <Header />
-     
+      <Header />
+      <CharacterGrid items={items} isLoading={isLoading}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/characters/CharactersGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CharactersGrid({isLoading,items}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CharactersGrid
